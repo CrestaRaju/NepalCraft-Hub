5f1a3286927d2d8f2a28175b64135c1f1b4d1a67
Raju Shrestha <rajutha007@gmail.com>
2026-03-30 16:17:28 +0530

    Delete server directory

--- server/src/routes/shipment.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const shipmentController = require('../controllers/shipment.controller');
-const { verifyToken, isAdmin } = require('../middleware/auth.middleware');
-
-router.post('/', [verifyToken, isAdmin], shipmentController.createShipment);
-router.get('/:orderId', verifyToken, shipmentController.getShipmentByOrder);
-
-module.exports = router;
