016bff02e633b437bc96b8eb98abdeb41389553e
Raju Shrestha <rajutha007@gmail.com>
2026-04-14 21:42:50 +0530

    Delete client directory

--- client/src/components/common/LoadingSpinner.jsx
+++
@@ -1,8 +0,0 @@
-const LoadingSpinner = ({ message = 'Loading...' }) => (
-  <div className="flex flex-col items-center justify-center py-20 gap-4">
-    <div className="spinner"></div>
-    <p className="text-gray-500 text-sm">{message}</p>
-  </div>
-);
-
-export default LoadingSpinner;
